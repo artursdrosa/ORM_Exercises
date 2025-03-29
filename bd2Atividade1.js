03a78894461ffa3669d2b7045692920a12d54da4
artursdrosa <artursr22@hotmail.com>
2025-03-29 15:04:27 -0300

    Finalizando as 3 funções para listar as 3 entidades

--- bd2Atividade1.js
+++ bd2Atividade1.js
@@ -31,28 +31,18 @@ Actor.belongsToMany(Film, { through: ActorFilm, foreignKey: 'actor_id' });
 ActorFilm.belongsTo(Actor, { foreignKey: 'actor_id' });
 ActorFilm.belongsTo(Film, { foreignKey: 'film_id' });
 
-/*let testConnection = async function(){
-  try {
-    let actorLoaded = await Actor.findByPk(1); 
-    let allFilmsPerActor = await actorLoaded.getFilms();
-    console.log(actorLoaded,allFilmsPerActor);
-    let filmLoaded = await Film.findByPk(1);
-    let allActorsPerFilm = await filmLoaded.getActors();
-    console.log(filmLoaded,allActorsPerFilm);
-    allFilmsPerActor = await ActorFilm.findAll({where: { actor_id: 1 }});
-    console.log("Filmes por ator: ", allFilmsPerActor);
-
-  } catch (error) {
-    console.error("Error log", error);
-  }
-}
-testConnection();*/
 
 let listActorsFilm = async function() {
     try{
-        let actorFilmLoaded = await ActorFilm.findOne({where:{film_id: 22}, include: [Actor, Film]});
-        console.log("\n\n\n===========ACTOR FILM LISTAGEM===========\n\n\n", JSON.stringify(actorFilmLoaded, null, 2));
-        //console.log("\n\n\n===========ACTOR LISTAGEM===========\n\n\n", JSON.stringify(actorFilmLoaded.dataValues.Actor.dataValues, null, 2));
+        let actorFilmCount = await ActorFilm.count();
+        let actorFilmLoaded = await ActorFilm.findAll({
+                          include: [Actor, Film],
+                          limit: 100});
+        console.log("\n\n\nA QUANTIDADE TOTAL DE ACTORFILM LISTADOS NA TABELA SÃO", actorFilmCount, "REGISTROS. SEGUE OS DADOS DOS 100 PRIMEIROS:");
+        for(let i= 1; i<101; i++){
+          console.log("\n\n===========ACTOR FILM", i, "===========\n\n");
+          console.log(JSON.stringify(actorFilmLoaded[i-1], null, 2));
+        }
     }catch (error){
         console.log("Error Log", error);
     }
@@ -60,12 +50,33 @@ let listActorsFilm = async function() {
 
 let listActor = async function () {
     try {
-        let actorLoaded = await Actor.findOne();
-        console.log("\n\n\n===========ACTOR LISTAGEM===========\n\n\n", JSON.stringify(actorLoaded, null, 2));
+        let actorCount = await Actor.count();
+        let actorLoaded = await Actor.findAll({limit: 100});
+
+        console.log("\n\n\nA QUANTIDADE TOTAL DE ACTOR LISTADOS NA TABELA SÃO", actorCount, "REGISTROS. SEGUE OS DADOS DOS 100 PRIMEIROS:");
+        for(let i= 1; i<101; i++){
+          console.log("\n\n===========ACTOR", i, "===========\n\n");
+          console.log(JSON.stringify(actorLoaded[i-1], null, 2));
+        }
     } catch (error) {
-        
+        console.log("Error Log", error);
+    }
+}
+
+let listFilm = async function () {
+    try {
+      let filmCount = await Film.count();
+      let filmLoaded = await Film.findAll({limit: 100});
+      console.log("\n\n\nA QUANTIDADE TOTAL DE FILM LISTADOS NA TABELA SÃO", filmCount, "REGISTROS. SEGUE OS DADOS DOS 100 PRIMEIROS:");
+      for(let i= 1; i<101; i++){
+        console.log("\n\n===========FILM", i, "===========\n\n");
+        console.log(JSON.stringify(filmLoaded[i-1], null, 2));
+      }
+    } catch (error) {
+        console.log("Error Log", error);
     }
 }
 
 //listActorsFilm();
-listActor();
+//listActor();
+listFilm();
