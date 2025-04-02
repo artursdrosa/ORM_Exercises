215740b7c2f50d96c8a7e9b9d33e952f07ea3483
Lucas de Oliveira <lucavarisco@hotmail.com>
2025-04-02 20:28:13 -0300

    Criando funções de Insert

--- bd2Atividade1.js
+++ bd2Atividade1.js
@@ -1,12 +1,17 @@
+const { title } = require('process');
+const { Json } = require('sequelize/lib/utils');
+const { parse } = require('path');
+const { read } = require('fs');
 const { Sequelize, DataTypes, where} = require('sequelize'); //npm install --save sequelize , npm install --save mysql2
 const MYSQL_IP="localhost";
 const MYSQL_LOGIN="root";
-const MYSQL_PASSWORD="1234";
+const MYSQL_PASSWORD="Computador.10";
 const DATABASE = "sakila";
 const sequelize = new Sequelize(DATABASE , MYSQL_LOGIN, MYSQL_PASSWORD, {
   host: MYSQL_IP,
   dialect: "mysql"
 });
+
 const Actor = sequelize.define('Actor', {
   actor_id: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   first_name: {type: DataTypes.STRING,allowNull: false },
@@ -17,7 +22,14 @@ const Actor = sequelize.define('Actor', {
   film_id: {type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true  },
   title: {type: DataTypes.STRING  },
   description: {type: DataTypes.STRING,  allowNull: false},
-  release_year: { type: DataTypes.DATE, }
+  release_year: { type: DataTypes.INTEGER },
+  language_id: {type: DataTypes.INTEGER, allowNull: false},
+  rental_duration: {type: DataTypes.INTEGER, allowNull: false},
+  rental_rate: {type: DataTypes.DOUBLE, allowNull: false},
+  length: {type: DataTypes.INTEGER, allowNull: false},
+  replacement_cost: {type: DataTypes.DOUBLE, allowNull: false},
+  rating: {type: DataTypes.STRING, allowNull: false},
+  special_features: {type: DataTypes.STRING, allowNull: false}
   }, { tableName: 'film',timestamps: false});
 
   const ActorFilm = sequelize.define('ActorFilm', {
@@ -77,6 +89,100 @@ let listFilm = async function () {
     }
 }
 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin, output: process.stdout
+});
+
+async function insertActor() {
+  rl.question("Digite o primeiro nome do ator: ", async (firstName) => {
+    rl.question("Digite o último nome do ator: ", async (lastName) => {
+      try {
+        const actor = await Actor.create({ first_name: firstName, last_name: lastName });
+        console.log("Ator inserido com sucesso:\n\n", JSON.stringify(actor, null, 2));
+      } catch (error) {
+        console.log("Erro ao inserir ator:", error);
+      }
+      rl.close();
+    });
+  });
+}
+
+function gerarNumAleatorio(min, max){
+  return(Math.random() * (max-min) + min).toFixed(2);
+}
+
+async function insertFilm() {
+  rl.question("Digite o título do Filme: ", async (titleFilm) => {
+    rl.question("Digite a descrição do Filme: ", async(descriptionFilm) => {
+      rl.question("Digite o ano de lançamento: ", async(releaseYear) => {
+        console.log("CATALOGO DE LINGUAS:\n 1 - Inglês \n 2 - Italiano \n 3 - Japonês \n 4 - Mandarim \n 5 - Francês \n 6 - Alemão");
+        rl.question("Digite o número referente a língua do filme: ", async (languageID) => {
+          rl.question("Digite por quanto tempo deseja ficar com o filme: ", async (rentalDuration) => {
+            const rental_rate = parseFloat(gerarNumAleatorio(0.99, 7.99));
+            console.log("Taxa de alguel: ", rental_rate);
+            const length = Math.floor(gerarNumAleatorio(60,240));
+            console.log("Tempo de filme: ", length);
+            const replacement_cost = parseFloat(gerarNumAleatorio(19.99, 69.99));
+            console.log("Taxa caso perda ou estragar: ", replacement_cost);
+            const rating = "PG" //definido como valor padrão
+            const special_features = "Trailers" //definido como valor padrão
+
+            try {
+              const film = await Film.create({
+                title: titleFilm,
+                description: descriptionFilm,
+                release_year: releaseYear,
+                language_id: languageID,
+                rental_duration: rentalDuration,
+                rental_rate,
+                length,
+                replacement_cost,
+                rating,
+                special_features
+              });
+              console.log("Filme inserido com sucesso:\n\n", JSON.stringify(film, null, 2));
+            } catch (error){
+              console.log("Erro ao inserir filme: ", error);
+            }
+            rl.close();
+          });
+        });
+      });
+    });
+  });
+}
+
+async function insertActorFilm() {
+  try {
+    const actorCount = await Actor.count();
+    const filmCount = await Film.count();
+
+    if (actorCount === 0 || filmCount === 0) {
+      console.log("Erro: Não há atores ou filmes suficientes no banco de dados.");
+      return;
+    }
+
+    const actor_id = Math.floor(gerarNumAleatorio(1, actorCount));
+    const film_id = Math.floor(gerarNumAleatorio(1, filmCount));
+
+    const actorFilm = await ActorFilm.create({
+      actor_id,
+      film_id
+    });
+
+    console.log("ID do filme e do ator inseridos com sucesso:\n", JSON.stringify(actorFilm, null, 2));
+  } catch (error) {
+    console.log("Erro ao inserir:", error);
+  } finally {
+    rl.close();
+  }
+}
+
 //listActorsFilm();
 //listActor();
-listFilm();
+//listFilm();
+//insertActor();
+//insertFilm();
+insertActorFilm();
